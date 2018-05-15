6c1f7b1d1e3fd6b7eb9c20ce9f2c3d85ee29b1bf
choitaehoon <xognsdl1230@naver.com>
2018-05-15 21:14:42 +0900

    없어진공간에 집어넣었고 search가 겹치는거와 없어진후 내려온 블락처리하기

--- kakao/src/friends.java
+++ kakao/src/friends.java
@@ -4,6 +4,7 @@ public class friends
 {
     public static int removeBlock(String[] answer)
     {
+        int count =0; //없어진 갯수 세기
         String[][] temp = new String[answer.length][answer[0].length()];//저장 변수
         boolean[][] search = new boolean[answer.length][answer[0].length()]; //탐색 변수
 
@@ -12,19 +13,25 @@ public class friends
                 if(answer[i].substring(j,j+1).equalsIgnoreCase(answer[i].substring(j+1,j+2)) && (answer[i+1].substring(j,j+1).equalsIgnoreCase(answer[i].substring(j,j+1)) &&
                 answer[i+1].substring(j+1,j+2).equalsIgnoreCase(answer[i].substring(j,j+1))))
                 {
-                    System.out.println(i+" "+j);
                     //같으면 탐색변수에 true로 저장
-                    search[i][j] = true; search[i][j+1] = true; //위
-                    search[i+1][j] =true; search[i+1][j+1] = true; //아래
-                }
+                    //중복된거 체크해야 하므로 false껏만 검사
 
+                        search[i][j] = true; count++; search[i][j+1] = true; count++;//위
+                        search[i+1][j] =true; count++; search[i+1][j+1] = true; count++; //아래
+                }
+                
         for(int i=0; i<answer.length; ++i)
             for(int j=0; j<answer[0].length(); ++j)
                 if(search[i][j])
                 {
-                    System.out.println(i+" "+j);
                     temp[i][j] = ""; //2x2 원소 없애기
                 }
+
+        for(int i=0; i<answer.length-1; ++i)
+            for(int j=0; j<answer[0].length(); ++j)
+                if(search[i+1][j])
+                    temp[i+1][j] = temp[i][j];
+
         return 0;
     }
 
