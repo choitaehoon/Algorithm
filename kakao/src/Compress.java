7bd95b30eca8f789314c5814f3014bfd5fd5e621
choitaehoon <xognsdl1230@naver.com>
2018-05-20 12:20:44 +0900

    버그 수정하기

--- kakao/src/Compress.java
+++ kakao/src/Compress.java
@@ -12,7 +12,7 @@ public class Compress
         //합친게 값이 있다면 그 값 사용하고, 없다면 그 한 글자만 사용하고 두 수를 더해 HashMap에 저장 한다.
         for(int i=0; i<temp.length()-1; ++i)
         {
-            if(i+1 == temp.length())
+            if(i+1 == temp.length()-1)
             {
                 array.add(hash.get(temp.substring(i+1,i+2)));
                 break;
@@ -36,6 +36,8 @@ public class Compress
     {
         for(int i=0; i<26; ++i)
             hash.put(String.valueOf((char)(i+65)),++count); //해쉬 함수에 1~26저장
-        System.out.println(compress("KAKAO"));
+//        System.out.println(compress("KAKAO"));
+//        array.clear();
+        System.out.println(compress("ABABABABABABABAB"));
     }
 }
