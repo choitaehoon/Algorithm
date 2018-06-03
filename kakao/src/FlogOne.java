e8eb53a5ba3bae681b9e165657d0be923fda5073
choitaehoon <xognsdl1230@naver.com>
2018-06-03 13:22:52 +0900

    FlogRiverOne버그수정함

--- kakao/src/FlogOne.java
+++ kakao/src/FlogOne.java
@@ -4,23 +4,26 @@ public class FlogOne
 {
     public static int jump(int x,int[] a)
     {
-        int[] temp = new int[a.length];//값을 넣어 체크하기
-        int count = 0;
-        for(int i=0; i<a.length; ++i) //temp에다가 그자리에 해당하는 값 넣기
-            if(temp[a[i]-1] == 0)
-            {
-                temp[a[i]-1] = a[i];
-                ++count;
-            }
+        int[] temp = new int[x+1];
+        int count =0;
+
         for(int i=0; i<a.length; ++i)
-            if(count == x && a[i] == x)
-                return i;
-        return -1;
+        {
+        if(temp[a[i]] == 0)
+        {
+            temp[a[i]] = a[i];
+            count ++;
+        }
+        if(count == temp.length-1)
+            return i;
+    }
+        return -1; //절대로 도달할 수 없을때
     }
 
     public static void main(String[] args)
     {
-        int[] a = {1,3,1,4,2,3,5,4};
-        System.out.println(jump(5,a));
+//        int[] a = {1,3,1,4,2,3,5,4};
+        int[] a = {2,2,2,2,2,2};
+        System.out.println(jump(2,a));
     }
 }
