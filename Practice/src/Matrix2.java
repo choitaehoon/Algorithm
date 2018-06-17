2369a4d5e0e387c2890d71b9dcc95118990268aa
choitaehoon <xognsdl1230@naver.com>
2018-06-17 16:25:36 +0900

    행렬곱셈 분석

--- Practice/src/Matrix2.java
+++ Practice/src/Matrix2.java
@@ -1,24 +1,46 @@
 public class Matrix2
 {
-    static int[][] temp = {
-            {6,7,12,5},
-            {5,3,11,18},
-            {7,17,3,3},
-            {8,10,14,9}
-    };
+    static class Matrix {
+        int row, col;
 
-    public static int min(int r,int c)
-    {
-        int a = 0;
-        if (r == 0 && c ==0) return temp[r][c];
-        else if(r == 0) a = min(r,c-1);
-        else if(c == 0) a = min(r-1,c);
-        else
-            a = Math.min(min(r,c-1),min(r-1,c));
-        return a+temp[r][c];
+        public Matrix(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
     }
 
-    public static void main(String[] args) {
-        System.out.println(min(3,3));
+    static int[][] temp;
+
+    static int 곱셈횟수최소값(Matrix[] a, int start, int end) throws Exception {
+        if(temp == null) temp = new int[a.length][a.length];
+        if(temp[start][end] != 0) return temp[start][end];
+        System.out.println("(start, end) "+start+" "+end);
+        if (start == end) return 0;
+        int 최소값 = Integer.MAX_VALUE;
+        for (int middle = start; middle < end; ++middle) {
+            int 횟수 = 곱셈횟수최소값(a, start, middle);
+            횟수 += 곱셈횟수최소값(a, middle + 1, end);
+            횟수 += a[start].row * a[middle].col * a[end].col;
+            if (횟수 < 최소값) 최소값 = 횟수;
+        }
+        return temp[start][end] = 최소값;
+    }
+
+    static int 곱셈횟수최대값(Matrix[] a, int start, int end) throws Exception {
+        if (start == end) return 0;
+        int 최대값 = 0;
+        for (int middle = start; middle < end; ++middle) {
+            int 횟수 = 곱셈횟수최소값(a, start, middle);
+            횟수 += 곱셈횟수최소값(a, middle + 1, end);
+            횟수 += a[start].row * a[middle].col * a[end].col;
+            if (횟수 > 최대값) 최대값 = 횟수;
+        }
+        return 최대값;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Matrix[] a = new Matrix[]{new Matrix(10, 100), new Matrix(100, 5), new Matrix(5, 50)};
+        System.out.println(곱셈횟수최소값(a, 0, a.length - 1));
+//        System.out.println(곱셈횟수최대값(a, 0, a.length - 1));
     }
 }
