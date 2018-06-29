3c67d2e089a9c06c6c7487609b2cd3148c78f46a
choitaehoon <xognsdl1230@naver.com>
2018-06-29 23:57:13 +0900

    BOJ1074

---
+++ Practice/src/Z.java
@@ -0,0 +1,19 @@
+
+public class Z
+{
+    static int search(int n, int r, int c) // n: 배열의 크기 r: 가로 c: 세로
+    {
+        int[][] temp = new int[2*n][2*n];
+        
+    }
+
+    static int search(int r, int c)
+    {
+
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println(search(2,3,1));
+    }
+}
