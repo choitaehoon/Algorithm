9884500749c1973ab81594c06b4eb013b87602d8
choitaehoon <xognsdl1230@naver.com>
2018-07-03 16:29:19 +0900

    회의실배정보류하기

---
+++ Practice/src/Meeting.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Meeting
+{
+    static class Me
+    {
+        int start;
+        int end;
+
+    }
+
+    public static void main(String[] args)
+    {
+        int[] temp = new int[1000];
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt(); //총 입력할 수
+
+    }
+}
