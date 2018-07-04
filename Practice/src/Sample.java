08bad5aee1202da21edb145351602220dac35b14
choitaehoon <xognsdl1230@naver.com>
2018-07-04 13:06:38 +0900

    BOJ2156 defer

---
+++ Practice/src/Sample.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Sample
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int[] d = new int [10001]; int[] a = new int[10001];
+    }
+}
