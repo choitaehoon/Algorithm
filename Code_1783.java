5f728e87f82784dfe2569a1c1123a539cea008f0
choitaehoon <xognsdl1230@naver.com>
2018-10-01 23:52:04 +0900

    practice

---
+++ Code_1783.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Code_1783
+{
+    /*
+     * 세로와 가로가 주어질 수 있는 수가 20억이기 때문에 for문으로 접근하면 안된다.
+     * 경우의 수를 나누어 풀어야 한다.
+     */
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
