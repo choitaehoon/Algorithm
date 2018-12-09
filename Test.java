fa50576bcaa92db6a36142ac5afa9f97bbba0e93
choitaehoon <xognsdl1230@naver.com>
2018-12-09 14:33:39 +0900

    practice

--- Test.java
+++ Test.java
@@ -1,18 +1,20 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Test
 {
-    public static void main(String[] args) {
-        for (int i=0; i<4; ++i)
-            for (int j=1; j<4; ++j)
-            {
-                if (j == 1)
-                    continue;
-                else
-                    System.out.println("gg");
+    public static void main(String[] args)
+    {
+        List<Integer> list = new ArrayList<>();
+        list.add(4);
+        list.add(2);
+        list.add(7);
+
+        list.sort(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return 0;
             }
+        });
+        Iterator iterator;
     }
 }
