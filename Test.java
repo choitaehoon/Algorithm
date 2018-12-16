848ec0998f487f1806102fd682cf09241b76470d
choitaehoon <xognsdl1230@naver.com>
2018-12-16 23:59:31 +0900

    practice

--- Test.java
+++ Test.java
@@ -1,67 +1,44 @@
-//import java.util.*;
-//
-//public class Test
-//{
-//    public static boolean next_permutation(int[] a) {
-//        int i = a.length-1;
-//        while (i > 0 && a[i-1] >= a[i]) {
-//            i -= 1;
-//        }
-//
-//        if (i <= 0) {
-//            return false;
-//        }
-//
-//        int j = a.length-1;
-//        while (a[j] <= a[i-1]) {
-//            j -= 1;
-//        }
-//
-//        int temp = a[i-1];
-//        a[i-1] = a[j];
-//        a[j] = temp;
-//
-//        j = a.length-1;
-//        while (i < j) {
-//            temp = a[i];
-//            a[i] = a[j];
-//            a[j] = temp;
-//            i += 1;
-//            j -= 1;
-//        }
-//        return true;
-//    }
-//    public static void main(String args[]) {
-//        Scanner sc = new Scanner(System.in);
-//        int n = sc.nextInt();
-//        int[][] a = new int[n][n];
-//        for (int i=0; i<n; i++) {
-//            for (int j=0; j<n; j++) {
-//                a[i][j] = sc.nextInt();
-//            }
-//        }
-//
-//        int[] d = new int[n-1];
-//        for (int i=0; i<n-1; i++) {
-//            d[i] = i+1;
-//        }
-//        int ans = Integer.MAX_VALUE;
-//        do {
-//            boolean ok = true;
-//            int sum = 0;
-//            for (int i=0; i<n-2; i++) {
-//                if (a[d[i]][d[i+1]] == 0) {
-//                    ok = false;
-//                } else {
-//                    sum += a[d[i]][d[i+1]];
-//                }
-//            }
-//            if (ok && a[0][d[0]] != 0 && a[d[n-2]][0] != 0) {
-//                sum += a[0][d[0]] + a[d[n-2]][0];
-//                ans = Math.min(ans, sum);
-//            }
-//        } while(next_permutation(d));
-//
-//        System.out.println(ans);
-//    }
-//}
+
+import java.util.*;
+
+public class Test
+{
+    public static void main(String[] args) {
+        Map<String,Integer> map = new HashMap<>();
+        map.put("a",1);
+        map.put("b",2);
+        map.put("c",3);
+
+        System.out.println("------------sort 전 -------------");
+        System.out.println(map);
+
+        Iterator it = Test.sortByValue(map).iterator();
+
+
+        System.out.println("---------sort 후------------");
+        while(it.hasNext()){
+            String temp = (String) it.next();
+            System.out.println(temp + " = " + map.get(temp));
+        }
+        //System.out.println(map);
+    }
+
+    public static List sortByValue(final Map map){
+        List<String> list = new ArrayList();
+        list.addAll(map.keySet());
+        Collections.sort(list,new Comparator(){
+
+            public int compare(Object o1,Object o2){
+                Object v1 = map.get(o1);
+                Object v2 = map.get(o2);
+
+                return ((Comparable) v1).compareTo(v2);
+            }
+
+        });
+        Collections.reverse(list); // 주석시 오름차순
+        return list;
+    }
+
+
+}
