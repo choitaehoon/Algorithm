6975313ad0ad23b73f9a805ddea46a325f162f58
choitaehoon <xognsdl1230@naver.com>
2021-08-23 17:25:52 +0900

    String 구현
    - 929. Unique Email Addresses

---
+++ src/leetcode/string/Main_929.java
@@ -0,0 +1,24 @@
+package leetcode.string;
+
+import java.util.*;
+
+public class Main_929 {
+
+    public int numUniqueEmails(String[] emails) {
+        Set<String> set = new HashSet<>();
+
+        for (int i = 0; i < emails.length; ++i) {
+            String[] email = emails[i].split("@");
+            String preEmail = email[0];
+            String postEmail = email[1];
+
+            String[] preEmailSplit = preEmail.split("\\+");
+            String resultPreEmail = preEmailSplit[0].replace(".", "");
+
+            set.add(resultPreEmail + "@" +postEmail);
+        }
+
+        return set.size();
+    }
+
+}
